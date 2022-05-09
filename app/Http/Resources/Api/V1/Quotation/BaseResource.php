90b964319515601cfd894b7fecde4ffd912a7618
Samir Fragozo <samirfragozob@gmail.com>
2022-05-09 03:31:24 +0200

    Quotation - BaseResource

---
+++ app/Http/Resources/Api/V1/Quotation/BaseResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources\Api\V1\Quotation;
+
+use App\Models\Quotation;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class BaseResource extends JsonResource
+{
+    public function __construct(Quotation $resource)
+    {
+        parent::__construct($resource);
+    }
+
+    public function toArray($request): array
+    {
+        $quotation = $this->resource;
+
+        return [
+            'total'        => $quotation->total,
+            'currency_id'  => $quotation->quotation_id,
+            'quotation_id' => $quotation->id,
+        ];
+    }
+}
