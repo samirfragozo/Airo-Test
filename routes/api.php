4c4d2eac70538f41bc177d333e964756ab55d512
Samir Fragozo <samirfragozob@gmail.com>
2022-05-09 06:14:28 +0200

    Quotation - Sanctum config

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,6 @@ use App\Http\Controllers\Api\V1\QuotationController;
 use Illuminate\Routing\Router;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('v1')->group(function (Router $route) {
+Route::prefix('v1')->middleware('auth:sanctum')->group(function (Router $route) {
     $route->post('/quotation', [QuotationController::class, 'store'])->name(RouteNames::API_V1_QUOTATION_POST);
 });
